c526ad6fa284fde528ed0b12808170dd269cc06d
Dynamo Denis <dmbugua66@gmail.com>
2020-04-02 08:13:20 +0300

    created a new date modifier pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { CustomDatePipe } from './custom-date.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     QuoteFormComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    CustomDatePipe
   ],
   imports: [
     BrowserModule,
