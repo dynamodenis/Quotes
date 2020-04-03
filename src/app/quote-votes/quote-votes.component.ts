b784b268501983d03e017642b337fbf64cdbe505
Dynamo Denis <dmbugua66@gmail.com>
2020-04-03 07:48:37 +0300

    added style binding to the upvote and downvote buttons

--- src/app/quote-votes/quote-votes.component.ts
+++ src/app/quote-votes/quote-votes.component.ts
@@ -10,12 +10,22 @@ export class QuoteVotesComponent implements OnInit {
   @Input() quote:Quotes;
   likes:number=0;
   dislikes:number=0;
+  isDisabled=false;
+  liked=false;
+
+  disliked=false;
 
   likeVotes(){
     this.likes++;
+    this.isDisabled=true;
+    this.liked=true;
+
   }
   downVotes(){
     this.dislikes++;
+    this.isDisabled=true;
+    this.disliked=true;
+    
   }
 
   
