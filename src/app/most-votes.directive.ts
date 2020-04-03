a1bea58567385935e759ea271dc4c7de841d318a
Dynamo Denis <dmbugua66@gmail.com>
2020-04-03 16:03:05 +0300

    created a regular attribute directive

--- src/app/most-votes.directive.ts
+++ src/app/most-votes.directive.ts
@@ -1,12 +1,18 @@
-import { Directive , ElementRef, Input} from '@angular/core';
+import { Directive , ElementRef, Input, OnInit} from '@angular/core';
 import { Quotes } from './quotes';
 
 @Directive({
   selector: '[appMostVotes]'
 })
-export class MostVotesDirective {
-  @Input() upvotes:Quotes;
-  @Input() downvotes:Quotes;
-  constructor(elem:ElementRef) { }
+export class MostVotesDirective implements OnInit{
+  @Input() quote:Quotes;
+  @Input() upVotes:number;
+  constructor(private elem:ElementRef) {
+      this.elem.nativeElement.style.backgroundColor='black';
+      this.elem.nativeElement.style.color="white"
+   }
 
+ngOnInit(){
+  
+}
 }
