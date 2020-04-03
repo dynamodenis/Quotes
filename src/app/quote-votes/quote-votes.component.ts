e96e1f6f966c2b4a26df5e51bba563b8257e1362
Dynamo Denis <dmbugua66@gmail.com>
2020-04-03 08:42:32 +0300

    added style binding to the votes buttons

--- src/app/quote-votes/quote-votes.component.ts
+++ src/app/quote-votes/quote-votes.component.ts
@@ -15,16 +15,41 @@ export class QuoteVotesComponent implements OnInit {
 
   disliked=false;
 
+  // Votes(){
+  //   if(this.liked=true){
+  //      this.likes++;
+  //   } else{
+  //     this.disliked=true
+  //     this.dislikes++;
+  //   }
+  // }
+
   likeVotes(){
-    this.likes++;
-    this.isDisabled=true;
-    this.liked=true;
+    // this.likes++;
+    // this.isDisabled=true;
+    // this.liked=true;
+    if(this.liked=true){
+      this.likes++;
+      this.disliked=false;
+      // this.isDisabled=true;
+    }else if(this.disliked=true){
+      this.liked=false
+      this.dislikes-1;
+    }
 
   }
   downVotes(){
-    this.dislikes++;
-    this.isDisabled=true;
-    this.disliked=true;
+    // this.dislikes++;
+    // this.isDisabled=true;
+    // this.disliked=true;
+    if(this.disliked=true){
+      this.dislikes++;
+      this.liked=false;
+    }else if(this.liked=true){
+      this.likes-1;
+      this.disliked=false;
+    }
+
     
   }
 
